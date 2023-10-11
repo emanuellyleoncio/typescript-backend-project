6bc08dad1a3bf4c188b1ad77b15678f6e83c99d9
emanuellyleoncio <manuleoncio01@gmail.com>
2023-10-11 07:41:06 -0400

    create GET endpoint

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,11 +1,27 @@
 import {Request, Response} from 'express';
+import {knex} from '../database/connection';
+import {Car} from '../types';
 
 export const getAllCars = async (req: Request, res: Response) => {
-
+    try {
+        const cars = await knex<Car>('cars');
+        return res.json(cars);
+    } catch (error: any) {
+        return res.status(400).json({ message: error.message });
+    }
 };
 
 export const getCar = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const car = await knex<Car>('cars').where({ id: Number(id) }).first();
 
+        if (!car) {
+            return res.status(404).json({ message: 'Car was not found!'})
+        }
+    } catch (error: any) {
+        return res.status(400).json({ message: error.message });
+    }
 };
 
 export const createCar = async (req: Request, res: Response) => {
