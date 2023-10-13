e436e72c8e040fbe13aedb484e8680d800bd0f4d
emanuellyleoncio <manuleoncio01@gmail.com>
2023-10-13 11:11:01 -0400

    add swagger on project

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import 'dotenv/config';
 import express from 'express';
 import routes from './routes';
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
+
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen('3000', () => {
     console.log('Server started!')
